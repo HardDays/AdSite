5b3587f5febb30e88b55872bdbdd849b9fd397f2
shvv733kn@gmail.com <shvv733kn@gmail.com>
2017-07-18 12:02:25 +0300

    toen_fix

--- app/models/token.rb
+++ app/models/token.rb
@@ -8,7 +8,8 @@ class Token < ApplicationRecord
 	end
 
 	def is_valid?
-		hours = (Time.now.utc - self.updated_at) / 1.hour
-		return hours <= 24
+		#hours = (Time.now.utc - self.updated_at) / 1.hour
+		#return hours <= 24
+		return true
 	end
 end
