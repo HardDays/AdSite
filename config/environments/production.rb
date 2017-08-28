9c5ab5ea09c97d220a92bc99a71dc67f93a3932f
shvv733kn@gmail.com <shvv733kn@gmail.com>
2017-08-28 13:19:23 +0300

    keke

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => 'patrimoinemail',
-    :password             => '123098rty',
+    :user_name            => ENV['GMAIL_USERNAME'],
+    :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
